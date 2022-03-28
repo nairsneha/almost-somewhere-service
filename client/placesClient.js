4943be91e859786658542ff99828f5ee5fc7fdbe
Shashwat <shashwatrathod22@gmail.com>
2022-03-28 17:02:19 -0400

    removed eslint disable

--- client/placesClient.js
+++ client/placesClient.js
@@ -8,8 +8,6 @@ import ClientResponse from './clientResponse';
 // With this instance, the query parameter 'key' will be automatically appended at the end
 // of our requests. We will not need to specify the 'key' seperately everytime.
 //  e.g placesInstance.get("/"), will make GET request to 'https://maps.googleapis.com/maps/api/place?key=`GCP_API_KEY`'
-// TODO: remove eslint-disable line after placesInstance is used.
-// eslint-disable-next-line no-unused-vars
 const placesInstance = axios.create({
   baseURL: 'https://maps.googleapis.com/maps/api/place',
   params: {
